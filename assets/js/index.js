8ec654c2579680a9b13663ecc749066af4300882
trjeremiahcelis <98722252+trjeremiahcelis@users.noreply.github.com>
2022-03-07 17:40:42 +0800

    commit on main
    new files

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,6 @@
-console.log("hello ")
+function toggleBurgerMenu() {
+    let navbarMenu = document.querySelector('.navbar-menu'); 
+        navbarMenu.classList.toggle('is-active');
+        navbarMenu.style.backgroundColor = "white";
+        navbarMenu.style.color = "black";
+    }
